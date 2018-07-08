f0c4abdecccd571c970461e915b7bd1431b3d2b4
mawguai <mawguai@gmail.com>
2018-07-09 01:48:42 +0200

    Step 6 Solution

--- js/app.js
+++ js/app.js
@@ -4,41 +4,65 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// create the shape
-let cubeGeometry = new THREE.BoxGeometry( 1, 1, 1);
-
-let cubeMaterials =
-    [
-        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } )
-    ];
-
-// Create the cube
-let cube = new THREE.Mesh( cubeGeometry, cubeMaterials );
-
+camera.position.set(0, 0, 70);
 control = new THREE.OrbitControls( camera, renderer.domElement );
 
-scene.add( cube );
+let floorGeometry = new THREE.PlaneBufferGeometry( 200, 200);
+let floorMaterials = new THREE.MeshPhongMaterial( {map : new THREE.TextureLoader().load('img/floor.jpg'), side : THREE.DoubleSide } );
+let floor = new THREE.Mesh( floorGeometry, floorMaterials );
+floor.position.set( 0, - 1, 0 );
+floor.rotation.x = - Math.PI * 0.5;
+floor.receiveShadow = true;
+floor.position.y = -11;
+scene.add( floor );
 
-camera.position.z = 3;
+// let ambientLight = new THREE.AmbientLight( 0xFFFFFF, 1.2 );
+var spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( 0, 80, 0 );
+spotLight.angle = 0.5;
+spotLight.penumbra = 0.1;
+spotLight.decay = 1.5;
+spotLight.intensity = 2;
+spotLight.distance = 200;
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.near = 10;
+spotLight.shadow.camera.far = 200;
+scene.add( spotLight );
 
-let ambientLight = new THREE.AmbientLight( 0xFFFFFF, 1.2 );
-let pointLight = new THREE.PointLight( 0xFF0040, 4, 50 );
+let ambient = new THREE.AmbientLight( 0xFFFFFF, 0.2 );
+scene.add( ambient );
 
-scene.add( ambientLight );
-scene.add( pointLight );
+let loader = new THREE.FontLoader();
+loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+    let textMaterial = new THREE.MeshLambertMaterial({color: 0x9e0ad8});
+
+    let textGeometry = new THREE.TextGeometry( 'Xebia', {
+        font: font,
+        size: 20,
+        height: 3,
+        curveSegments: 20,
+        bevelEnabled: false,
+        bevelThickness: 2,
+        bevelSize: 2,
+        bevelSegments: 10
+    } );
+
+    let textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+    textMesh.position.set( -35, -10, 0);
+
+    scene.add( textMesh );
+} );
+
+
 
 // Game logic
 let update = function()
 {
-    let time = Date.now() * 0.0005;
-    pointLight.position.x = Math.sin( time * 0.7 ) * 30;
-    pointLight.position.y = Math.sin( time * 0.5 ) * 40;
-    pointLight.position.z = Math.sin( time * 0.3 ) * 30;
+
 };
 
 // Game draw
