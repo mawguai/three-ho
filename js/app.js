746388710443a586dbd73839db1c6d35afad7123
mawguai <mawguai@gmail.com>
2018-07-09 01:33:33 +0200

    Step 2 Solution

--- js/app.js
+++ js/app.js
@@ -4,8 +4,38 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// draw
+// create the shape
+let geometry = new THREE.BoxGeometry( 1, 1, 1);
+
+// Create a material
+let material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true });
+
+// Create the cube
+let cube = new THREE.Mesh( geometry, material );
+
+scene.add( cube );
+
+camera.position.z = 3;
+
+// Game logic
+let update = function()
+{
+    cube.rotation.x += 0.01;
+};
+
+// Game draw
 let render = function()
 {
     renderer.render( scene, camera );
 };
+
+// run Game Loop (update, render, repeat)
+let gameLoop = function()
+{
+    requestAnimationFrame( gameLoop );
+
+    update();
+    render();
+};
+
+gameLoop();
