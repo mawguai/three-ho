ac6f73fe768110c6d5abb5481d409001b2a4463e
mawguai <mawguai@gmail.com>
2018-07-09 01:45:10 +0200

    Step 5 Solution

--- js/app.js
+++ js/app.js
@@ -5,23 +5,20 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 // create the shape
-let geometry = new THREE.BoxGeometry( 1, 1, 1);
+let cubeGeometry = new THREE.BoxGeometry( 1, 1, 1);
 
 let cubeMaterials =
     [
-        new THREE.MeshBasicMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.FrontSide } ),
-        new THREE.MeshBasicMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshBasicMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshBasicMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshBasicMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
-        new THREE.MeshBasicMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } )
+        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
+        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
+        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
+        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
+        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } ),
+        new THREE.MeshLambertMaterial( {map : new THREE.TextureLoader().load('img/panda_roux.jpg'), side : THREE.DoubleSide } )
     ];
 
-// Create a material
-let material = new THREE.MeshFaceMaterial( cubeMaterials );
-
 // Create the cube
-let cube = new THREE.Mesh( geometry, material );
+let cube = new THREE.Mesh( cubeGeometry, cubeMaterials );
 
 control = new THREE.OrbitControls( camera, renderer.domElement );
 
@@ -29,13 +26,19 @@ scene.add( cube );
 
 camera.position.z = 3;
 
+let ambientLight = new THREE.AmbientLight( 0xFFFFFF, 1.2 );
+let pointLight = new THREE.PointLight( 0xFF0040, 4, 50 );
 
+scene.add( ambientLight );
+scene.add( pointLight );
 
 // Game logic
 let update = function()
 {
-    //cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
+    let time = Date.now() * 0.0005;
+    pointLight.position.x = Math.sin( time * 0.7 ) * 30;
+    pointLight.position.y = Math.sin( time * 0.5 ) * 40;
+    pointLight.position.z = Math.sin( time * 0.3 ) * 30;
 };
 
 // Game draw
