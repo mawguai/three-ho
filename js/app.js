7fc24adc4676c18efc04cef733f45b10e25621a0
mawguai <mawguai@gmail.com>
2018-07-09 02:08:30 +0200

    Step 2 exercise

--- js/app.js
+++ js/app.js
@@ -4,23 +4,21 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// create the shape
-let geometry = new THREE.BoxGeometry( 1, 1, 1);
+// TODO 1 : create the shape, here we are creating a box
+let geometry =
 
-// Create a material
-let material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true });
+// TODO 2 : Add a colour to our box, use a MeshBasicMaterial, to see it
+let material =
 
-// Create the cube
-let cube = new THREE.Mesh( geometry, material );
+// TODO 3 : associate the material and the shape in a mesh
+let cube =
 
-scene.add( cube );
-
-camera.position.z = 3;
+// TODO 4 : add the cube to the scene
 
 // Game logic
 let update = function()
 {
-    cube.rotation.x += 0.01;
+    // TODO 5 : BONUS add some rotations to the cube to see the 3D aspect (to see it better update the wireframe property of the material)
 };
 
 // Game draw
@@ -32,7 +30,7 @@ let render = function()
 // run Game Loop (update, render, repeat)
 let gameLoop = function()
 {
-    requestAnimationFrame( gameLoop );
+    requestAnimationFrame( gameLoop ); // a tick operation provided by webGl
 
     update();
     render();
